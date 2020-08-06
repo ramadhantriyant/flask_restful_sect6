f9df8ae9e19679f2015cdd55f92156cc0ff8e05e
prabuniwatakawaca <--list>
2020-08-06 21:32:27 +0700

    Initial commit

---
+++ resources/store.py
@@ -0,0 +1,40 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.store import StoreModel
+
+class Store(Resource):
+    @jwt_required()
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
+
+        if store:
+            return store.json()
+        return {'message': f"Store {name} doesn't exists!"}
+
+    @jwt_required()
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': f"Store {name} is already exists!"}, 400
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except:
+            return {'message': "An error occured when inserting the store"}, 500
+        return store.json(), 201
+
+    @jwt_required()
+    def delete(self, name):
+        if StoreModel.find_by_name(name):
+            store = StoreModel(name)
+            try:
+                store.delete_from_db()
+            except:
+                return {'message': "An error occured when deleting the store"}, 500
+            return {'message': f"Store {name} deleted!"}, 201
+
+        return {'message': f"Store {name} doesn't exists!"}, 404
+
+class StoreList(Resource):
+    @jwt_required()
+    def get(self):
+        return {'stores': [store.json() for store in StoreModel.query.all()]}
